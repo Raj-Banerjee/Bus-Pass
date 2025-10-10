bc4a0598c9442a29b25de0674bbbde27388d3c20
Raj Banerjee <rajbanerjee978@gmail.com>
2025-10-10 10:56:31 +0530

    Initial Commit

---
+++ src/landingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from './components/banner'
+
+const landingPage = () => {
+  return (
+    <>
+    <Banner/>
+    <div>landingPage here</div>
+    </>
+  )
+}
+
+export default landingPage
