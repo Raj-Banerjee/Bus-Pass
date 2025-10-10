500a578c8b7d7534ea265999b49fa12426dabce2
Raj Banerjee <rajbanerjee978@gmail.com>
2025-10-10 11:32:24 +0530

    Installing and implimenting Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -6,6 +7,7 @@ export default defineConfig({
   plugins: [
     react({
     jsxRuntime: 'automatic',
-  })
+  }),
+  tailwindcss(),
 ],
 })
