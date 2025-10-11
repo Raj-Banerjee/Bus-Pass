13423ddff2ff6da3ec2875c305264ede5cf4b96e
Raj Banerjee <rajbanerjee978@gmail.com>
2025-10-11 10:23:07 +0530

    Banner Implimentation of Tailwind

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import DehazeIcon from '@mui/icons-material/Dehaze';
 
 const banner = () => {
   return (
     <div>
-
-        
+      <div className="grid grid-cols-12">
+        <div className="col-span-4"><DehazeIcon/></div>
+        <div className="col-span-4">TCS Bus Pass</div>
+        <div className="col-span-4">Login</div>
+      </div>
     </div>
   )
 }
